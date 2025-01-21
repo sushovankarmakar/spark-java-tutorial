9085a3a849d489eb0a6a9964b9387414bd8f2502
Sushovan Karmakar <sushovankarmakar1996@gmail.com>
2025-01-21 08:06:26 +0530

    Simplify RDD transformations using modern Java syntax
    Replaced lambda expressions with method references for improved readability and adherence to modern Java practices. This change makes the code more concise and easier to understand without altering functionality.

--- src/main/java/udemy/_2_Function_Map.java
+++ src/main/java/udemy/_2_Function_Map.java
@@ -30,19 +30,21 @@ public class _2_Function_Map {
         JavaSparkContext sc = new JavaSparkContext(sparkConf);
         JavaRDD<Integer> myRDD = sc.parallelize(inputData);
 
-        JavaRDD<Double> squareRoots = myRDD.map((value) -> Math.sqrt(value));
-        // JavaRDD<Double> squareRoots = myRDD.map(Math::sqrt); // more modern syntax, when we're passing one value and operating on one value only, then we can use this modern syntax
+        //JavaRDD<Double> squareRoots = myRDD.map((value) -> Math.sqrt(value));
+        JavaRDD<Double> squareRoots = myRDD.map(Math::sqrt); // more modern syntax, when we're passing one value and operating on one value only, then we can use this modern syntax
         squareRoots.foreach(val -> System.out.println("Sqrt val : " + val));
 
         //squareRoots.foreach(System.out::println); // more modern syntax - this will give me 'not serializable exception' - because before sending println function to RDDs, spark tries to serialize it but println function in java is NOT serializable.
         squareRoots.collect().forEach(System.out::println); // this is a fix of the above line.
 
+        // ------------------------------------------------------------------------------------------------------------
         // how many elements in RDD ? - two ways we can do
         // 1. to count the number of items inside the RDD, we can use .count() operation, but it should be use if we're at the end of the process
         System.out.println(squareRoots.count());
 
         // 2. we can use map and reduce
-        Long totalNumOfElements = squareRoots.map(val -> 1L).reduce((val1, val2) -> val1 + val2); // use long type instead of integer, cause in big data, integer can be max 2 billion.
+        //Long totalNumOfElements = squareRoots.map(val -> 1L).reduce((val1, val2) -> val1 + val2); // use long type instead of integer, cause in big data, integer can be max 2 billion.
+        Long totalNumOfElements = squareRoots.map(val -> 1L).reduce(Long::sum); // more modern syntax
         System.out.println("Total number of elements are : " + totalNumOfElements);
 
         sc.close();
