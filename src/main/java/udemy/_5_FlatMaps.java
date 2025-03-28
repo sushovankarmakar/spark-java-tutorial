c7e48f9d197bbdee75d4828a2dcf667721ef0387
Sushovan <officialkarmakar@gmail.com>
2025-03-28 13:06:46 +0530

    added slf4j dependency

---
+++ src/main/java/udemy/_5_FlatMaps.java
@@ -0,0 +1,40 @@
+package udemy;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class _5_FlatMaps {
+
+    public static void main(String[] args) {
+
+        List<String> inputData = new ArrayList<>();
+        inputData.add("WARN: Tuesday 4 September 0405");
+        inputData.add("ERROR: Tuesday 4 September 040");
+        inputData.add("FATAL: Wednesday 5 September 0405");
+        inputData.add("ERROR: Tuesday 4 September 0405");
+        inputData.add("WARN: Tuesday 4 September 0405");
+
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+        SparkConf sparkConf = new SparkConf()
+                .setAppName("flatMaps-examples")
+                .setMaster("local[*]");
+
+        try (JavaSparkContext jsc = new JavaSparkContext(sparkConf)) {
+
+            JavaRDD<String> inputRdd = jsc.parallelize(inputData);
+            JavaRDD<String> words = inputRdd.flatMap(val ->
+                    Arrays.asList(val.split(" ")).iterator()
+            );
+
+            words.foreach(word ->  System.out.println(word + " "));
+        }
+    }
+}
